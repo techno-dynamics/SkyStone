0b737765a804c0483f5bd5ffdb8d447b5f20abf9
Brian <brian@arct.io>
2019-09-08 12:05:30 -0700

    Version 1.1.0
    Getting ready for season

---
+++ TeamCode/src/main/java/io/arct/ftclogic/Version_1_1_0.kt
@@ -0,0 +1,10 @@
+package io.arct.ftclogic
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous
+import io.arct.ftccore.OperationMode
+
+@Autonomous
+class Version_1_1_0 : OperationMode() {
+    override fun init() {}
+    override fun loop() {}
+}
