184c4c39d9714a10aefe5f8cb8b20efcec02e6c0
arktfox <brian@arct.io>
2020-02-07 17:53:08 -0800

    New motor positioning system

--- TeamCode/src/main/java/io/arct/ftclogic/Test.kt
+++ TeamCode/src/main/java/io/arct/ftclogic/Test.kt
@@ -1,59 +1,53 @@
 package io.arct.ftclogic
 
-import io.arct.ftc.Vuforia
-import io.arct.ftc.Vuforia.Constants.mmPerInch
+import io.arct.ftclib.drive.MecanumDrive
 import io.arct.ftclib.eventloop.LinearOperationMode
 import io.arct.ftclib.eventloop.OperationMode
-import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix
-import org.firstinspires.ftc.robotcore.external.matrices.VectorF
-import org.firstinspires.ftc.robotcore.external.navigation.*
+import io.arct.ftclib.hardware.motors.FtcMotor
+import io.arct.robotlib.drive.Drive
+import io.arct.robotlib.hardware.motors.Motor
+import io.arct.robotlib.robot.device
 
 @OperationMode.Bind(OperationMode.Type.Autonomous, group = "Calibration")
 class Test : LinearOperationMode() {
-    private var lastLocation: OpenGLMatrix? = null
-    private var targetVisible: Boolean = false
+
+    init {
+        FtcMotor.adjustmentPower = 0.1
+        FtcMotor.targetPositionTolerance = 0.0
+    }
+
+    private var drive: Drive = MecanumDrive(robot,
+        robot device "motor0",
+        robot device "motor3",
+        robot device "motor1",
+        robot device "motor2",
+
+        alignment = 180.0
+    )
 
     override fun run() {
-        val tracker = Vuforia.build(BuildConfig.VUFORIA_KEY, robot)
+        val m: Motor = robot device "motor0"
 
-        tracker.targets.activate()
+        m.move(0.2, -100.0); Thread.sleep(100)
+        m.move(0.4, 200.0); Thread.sleep(100)
+        m.move(0.6, -300.0); Thread.sleep(100)
+        m.move(0.8, 400.0); Thread.sleep(100)
+        m.move(1.0, -500.0); Thread.sleep(100)
 
-        while (active && !stopRequested) {
-            targetVisible = false
+        Thread.sleep(2000)
 
-            for (trackable in tracker.trackables) {
-                if ((trackable.listener as VuforiaTrackableDefaultListener).isVisible) {
-                    log.add("Visible Target: ${trackable.name}")
-                    targetVisible = true
+        drive.move(0.0, 0.2, 100.0); Thread.sleep(100)
+        drive.move(90.0, 0.4, 200.0); Thread.sleep(100)
+        drive.move(180.0, 0.6, 300.0); Thread.sleep(100)
+        drive.move(-90.0, 0.8, 400.0); Thread.sleep(100)
+        drive.move(0.0, 1.0, 500.0); Thread.sleep(100)
 
-                    val robotLocationTransform = (trackable.listener as VuforiaTrackableDefaultListener).updatedRobotLocation
+        Thread.sleep(2000)
 
-                    if (robotLocationTransform != null)
-                        lastLocation = robotLocationTransform
-
-                    break
-                }
-            }
-
-            if (targetVisible) {
-                val translation: VectorF = lastLocation!!.translation
-
-                val x = translation[0] / mmPerInch
-                val y = translation[1] / mmPerInch
-                val z = translation[2] / mmPerInch
-
-                log.add("Pos (in) {X, Y, Z} = $x, $y, $z")
-
-                val rotation = Orientation.getOrientation(lastLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES)
-
-                log.add("Rot (deg) {Roll, Pitch, Heading} = ${rotation.firstAngle}, ${rotation.secondAngle}, ${rotation.thirdAngle}")
-            } else {
-                log.add("Visible Target: none")
-            }
-
-            log.update()
-        }
-
-        tracker.targets.deactivate()
+        drive.rotate(0.2, 100.0); Thread.sleep(100)
+        drive.rotate(0.4, -200.0); Thread.sleep(100)
+        drive.rotate(0.6, 300.0); Thread.sleep(100)
+        drive.rotate(0.8, -400.0); Thread.sleep(100)
+        drive.rotate(1.0, 500.0); Thread.sleep(100)
     }
 }
