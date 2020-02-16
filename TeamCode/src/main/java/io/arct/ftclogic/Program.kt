e346ffe9b3342681589661436a213cd77717a72e
arctfoxx <brian@arct.io>
2020-02-16 15:39:16 -0800

    Almost There!

--- TeamCode/src/main/java/io/arct/ftclogic/Program.kt
+++ TeamCode/src/main/java/io/arct/ftclogic/Program.kt
@@ -5,6 +5,7 @@ import io.arct.ftc.StoneState
 import io.arct.ftclib.drive.MecanumDrive
 import io.arct.ftclib.eventloop.LinearOperationMode
 import io.arct.ftclib.eventloop.OperationMode
+import io.arct.robotlib.drive.Drive
 import io.arct.robotlib.hardware.motors.Motor
 import io.arct.robotlib.hardware.motors.Servo
 import io.arct.robotlib.navigation.Direction
@@ -88,10 +89,11 @@ class Program : LinearOperationMode() {
     }
 
     override fun run() = when (mode) {
-        0 -> blue()
-        1 -> red()
+        0 -> program(0, Direction.Backward, 1.0)
+        1 -> program(0, Direction.Forward, -1.0)
         2 -> neutral()
-        else -> throw Exception("Imagine if autonomous didn't work")
+
+        else -> Unit
     }
 
     private fun neutral() {
@@ -100,126 +102,86 @@ class Program : LinearOperationMode() {
         tape.power = 0.0
     }
 
-    private fun blue() {
+    private fun program(mode: Int, directionBuild: Direction, rotationModifier: Double) {
         val position = detector.state
 
         drive.move(Direction.Right, 0.2, 45.0)
 
         val offset = when (position) {
             StoneState.One -> -17.5
-            StoneState.Two -> -2.0
+            StoneState.Two -> -1.5
             StoneState.Three -> 10.0
 
             StoneState.Unknown -> -2.0
         }
 
         if (offset > 0.0)
-            drive.move(Direction.Forward, 0.2, offset)
+            drive.move(Direction.Forward, .2, offset)
         else
-            drive.move(Direction.Backward, 0.2, abs(offset))
+            drive.move(Direction.Backward, .2, abs(offset))
 
         // Get Stone
         getStone()
 
         // Move Back
 
-        drive.move(Direction.Backward, 0.4, 120.0 + offset)
+        drive.move(directionBuild, .4, 120.0 + (offset * rotationModifier))
         releaseStone()
 
-        drive.move(Direction.Backward, 0.25, 70.0)
-        drive.rotate(-0.2, 80.0)
+        drive.move(directionBuild, .25, 65.0)
 
-        drive.move(Direction.Backward, 0.2, 45.0)
-        buildplate(true)
-
-        drive.move(Direction.Forward, 0.4, 30.0)
-
-        drive.rotate(0.4, 20.0)
-        drive.move(Direction.Forward, 0.4, 20.0)
-        drive.rotate(0.4, 95.0)
-
-        buildplate(false)
-
-        drive.move(Direction.Backward, 0.3)
-        Thread.sleep(1250)
+        // WALL ALIGN {
+        drive.move(Direction.Left, 0.2)
+        Thread.sleep(2000)
         drive.stop()
-
-        tape.power = -75.0
-        drive.move(Direction.Forward, 0.75, 30.0)
-
-        Thread.sleep(450)
-    }
-
-    private fun red() {
-        val position = detector.state
-
         drive.move(Direction.Right, 0.2, 45.0)
+        // }
 
-        val offset = when (position) {
-            StoneState.One -> 10.0
-            StoneState.Two -> 2.0
-            StoneState.Three -> -17.5
+        drive.rotate(-.2, 70.0)
 
-            StoneState.Unknown -> -2.0
-        }
-
-        if (offset > 0.0)
-            drive.move(Direction.Forward, 0.2, offset)
-        else
-            drive.move(Direction.Backward, 0.2, abs(offset))
-
-        // Get Stone
-        getStone()
-
-        // Move Forward
-
-        drive.move(Direction.Forward, 0.4, 120.0 + offset)
-        releaseStone()
-
-        drive.move(Direction.Forward, 0.25, 70.0)
-        drive.rotate(-0.2, 80.0)
-
-        drive.move(Direction.Backward, 0.2, 45.0)
+        drive.move(Direction.Backward, .2, 40.0)
         buildplate(true)
 
-        drive.move(Direction.Forward, 0.4, 30.0)
+        drive.move(Direction.Forward, .4, 30.0)
 
-        drive.rotate(0.4, 20.0)
-        drive.move(Direction.Backward, 0.4, 20.0)
-        drive.rotate(0.4, 95.0)
+        drive.rotate(rotationModifier * .4, 20.0)
+        drive.move(Direction.Forward, .4, 20.0)
+        drive.rotate(rotationModifier * .4, 95.0)
 
         buildplate(false)
 
-        drive.move(Direction.Backward, 0.3)
+        drive.move(Direction.Backward, .3)
         Thread.sleep(1250)
         drive.stop()
 
-        tape.power = -75.0
-        drive.move(Direction.Forward, 0.75, 30.0)
+        if (mode == 0)
+            drive.move(Direction.Right, 0.2, 30.0)
 
-        Thread.sleep(450)
+        tape.power = -.75
+        drive.move(Direction.Forward, 0.1, 10.0)
+        Thread.sleep(1750)
     }
 
     private fun buildplate(buildplate: Boolean) {
-        buildplateLeft.position = if (buildplate) 1.0 else 0.0
-        buildplateRight.position = if (buildplate) 0.0 else 1.0
+        buildplateLeft.position = if (buildplate) 1.0 else .0
+        buildplateRight.position = if (buildplate) .0 else 1.0
         Thread.sleep(1000)
     }
 
     private fun getStone() {
-        drive.move(Direction.Right, 0.1, 45.0)
+        drive.move(Direction.Right, .1, 45.0)
         grabStone()
 
-        drive.move(Direction.Left, 0.2, 45.0)
+        drive.move(Direction.Left, .2, 45.0)
     }
 
     private fun grabStone() {
-        grabber.position = 0.0
+        grabber.position = .0
         Thread.sleep(1000)
     }
 
     private fun releaseStone() {
         grabber.position = 1.0
-        Thread.sleep(1000)
+        Thread.sleep(500)
     }
 }
